826f12c1961bd943f4078d32bff4327abfc1dda8
tony_luo <tony.luo.2022.08@gmail.com>
2023-02-14 16:22:37 +0800

    misc: Add template

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
 const express = require('express')
 require('dotenv').config();
 const axios = require('axios')
-const cli = require('./cli.json')
 
 const app = express()
 app.use(express.json())
 
 const port = process.env.PORT
+const tgbot = process.env.TGBOT
 
 app.post('/api/tg', async (req, res) => {
-    const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
+    // ======= Test =======  
+    console.log(req.body)
 
-    cli.commands.map(async(e,i)=>{
-        if (req.body.message.text === '/'+cli.cli_name+" "+cli.commands[i].command) {
-            let body = {
-                chat_id: req.body.message.chat.id,
-                text: cli.commands[i].body,
-            }
-            let headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            }
-            await axios({
-                url: `https://api.telegram.org/bot${tgbot}/sendMessage`,
-                method: "POST",
-                headers: headers,
-                data: body
-            })
-    
-            await axios(`${cli.commands[i].target_url}`)
-            .catch(err=>{
-                console.log(err)
-            })
-        }
-    })
-    
-      
+    // ======= Send message to group ======= 
+    // const body = {
+    //     chat_id: req.body.message.chat.id,
+    //     text: "你好 @"+req.body.message.from.username,
+    // }
+    // const headers = {
+    //     "Content-Type": "application/json",
+    //     "Accept": "application/json",
+    // }
+    // await axios({
+    //     url: `https://api.telegram.org/bot${tgbot}/sendMessage`,
+    //     method: "POST",
+    //     headers: headers,
+    //     data: body
+    // })
+   
     res.status(200).send('OK');
 })
 
